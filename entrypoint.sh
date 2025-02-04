01358cd259680be1fc5de5d65d45b1caae878cd9
Jim Carroll <pcarroll@mirantis.com>
2025-02-04 16:34:00 +1300

    chore: Improve output messages and formatting in entrypoint script

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,23 +5,23 @@ COUNT=""
 
 while [ "$#" -gt 0 ]; do
   case "$1" in
-    -v|--verbose)
-      VERBOSE="-v"
-      shift
-      ;;
-    -c|--count)
-      if [ -n "$2" ] && [ "$2" -eq "$2" ] 2>/dev/null; then
-        COUNT="$2"
-        shift 2
-      else
-        echo "Error: Argument for $1 must be an integer." >&2
-        exit 1
-      fi
-      ;;
-    *)
-      echo "Unknown option: $1" >&2
+  -v | --verbose)
+    VERBOSE="-v"
+    shift
+    ;;
+  -c | --count)
+    if [ -n "$2" ] && [ "$2" -eq "$2" ] 2>/dev/null; then
+      COUNT="$2"
+      shift 2
+    else
+      echo "Error: Argument for $1 must be an integer." >&2
       exit 1
-      ;;
+    fi
+    ;;
+  *)
+    echo "Unknown option: $1" >&2
+    exit 1
+    ;;
   esac
 done
 
@@ -29,7 +29,7 @@ if [ -n "$COUNT" ]; then
   export COUNT
 fi
 
-echo "Creating LDIF based on environment variables..."
+echo "Creating LDIF based on the COUNT environment variable (default: 100)..."
 GEN_OUT=$(python /ldap/ldifgen.py)
 echo "${GEN_OUT}"
 
@@ -37,6 +37,8 @@ echo "Initializing LDAP database from /ldap/init.ldif..."
 rm -rf /var/lib/openldap/data/*
 slapadd $VERBOSE -l /ldap/init.ldif
 
+# Repeat the output, in the event that the user specified -v as this makes for a chatty slapadd run.
+# Saves the user from having to scroll up to see the output.
 echo
 echo "Replaying output of Python LDIF generator..."
 echo "${GEN_OUT}"
